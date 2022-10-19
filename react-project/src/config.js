c24dadf8570254b4d316d4c49a9c42fc6e308899
Muhammad Umair Ahmed <muhdumairahmed@gmail.com>
2022-10-20 00:09:02 +0300

    Make parent container padding relative

--- react-project/src/config.js
+++ react-project/src/config.js
@@ -4,8 +4,8 @@ export const legendSvgId = "d3_legend_svg";
 export const legendSizeSvgId = "d3_legend_size_svg";
 export const LAYOUT_CONSTANTS = {
     treemap: {
-        overallPadding: 10,
-        topPadding: 80,
+        overallPadding: window.innerHeight * 0.01,
+        topPadding: window.innerHeight * 0.05,
         height: window.innerHeight,
         width: window.innerWidth * 0.65,
         max_depth: 2,
