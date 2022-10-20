2a26a7914697d6e0d2a78fceedad22be8413b2e5
Muhammad Umair Ahmed <muhdumairahmed@gmail.com>
2022-10-21 00:19:57 +0300

    remove redundant reducer definition

--- react-project/src/store.js
+++ react-project/src/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
-import defaultSlice from './reducers/defaultSlice'
 import filterSlice from './reducers/filterSlice'
 import treemapSlice from './reducers/treemapSlice'
 
 export const store = configureStore({
     reducer: {
-        // default: defaultSlice,
         treemap: treemapSlice,
         filter: filterSlice
     },
