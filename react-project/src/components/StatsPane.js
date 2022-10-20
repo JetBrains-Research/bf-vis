19336892f2d44102679264e51f1d9f12c5f689d2
Muhammad Umair Ahmed <muhdumairahmed@gmail.com>
2022-10-21 00:19:42 +0300

    Fix author box-sizing issue

--- react-project/src/components/StatsPane.js
+++ react-project/src/components/StatsPane.js
@@ -1,19 +1,17 @@
 import React, { useState } from "react";
 import * as d3 from 'd3';
-import { min } from "d3";
 
 function StatsPane(props) {
     const formatPercentage = d3.format(",.1%");
     const formatSI = d3.format(".3s");
-    
+
     const nodeData = props.data;
     const nodeBusFactor = ("busFactor" in nodeData.busFactorStatus) ? nodeData.busFactorStatus.busFactor : "N/A";
     const authorsList = ("usersFormatted" in nodeData) ? [...nodeData.usersFormatted] : undefined;
     let topAuthors = undefined;
     let authorsListContributionPercentage = undefined;
     const totalNumOfAuthors = (authorsList) ? authorsList.length : 0;
-    const initialNumOfAuthors = min([totalNumOfAuthors, 3]);
-    const [numOfAuthors, setNumOfAuthors] = useState(initialNumOfAuthors);
+    const [numOfAuthors, setNumOfAuthors] = useState(0);
 
     if (authorsList) {
         authorsList.sort((a, b) => b.contributionScore - a.contributionScore);
@@ -26,7 +24,6 @@ function StatsPane(props) {
                 "relativeScore": authorContributionPair.contributionScore / cumulativeAuthorContributionScore
             }
         });
-
         topAuthors = authorsListContributionPercentage.slice(0, numOfAuthors);
     }
 
@@ -35,20 +32,37 @@ function StatsPane(props) {
             <h4>Stats</h4>
             <div className="col-12" >
                 <p className="small">Here are some details about the selected node</p>
+
                 <h5>Bus Factor</h5>
                 <p className="small">The bus factor of <span className="fw-italic">{nodeData.name}</span> is: <span
                     className="fw-bold">{nodeBusFactor}</span></p>
+
                 <h5>Author Contribution</h5>
-                <label htmlFor="authorNumberSelecter" className="form-label">Number of Authors to show: {numOfAuthors} </label>
-                <input type="range" className="form-range" defaultValue={numOfAuthors} onChange={(e) => setNumOfAuthors(e.target.value)} min="1" max={totalNumOfAuthors} id="authorNumberSelecter"></input>
-                <div className="list-group list-group-flush overflow-scroll" style={{
-                    "maxHeight": "30vh",
-                    "maxWidth": "14vw"
+                {
+                    (authorsList && topAuthors) ?
+                        <>
+                            <label htmlFor="authorNumberSelecter" className="form-label">Number of Authors to show: {numOfAuthors} </label>
+                            <input type="range" className="form-range" defaultValue={Math.min(totalNumOfAuthors, 3)} onChange={(e) => setNumOfAuthors(e.target.value)} min="0" max={totalNumOfAuthors} id="authorNumberSelecter">
+                            </input>
+                        </> :
+                        <>
+                            <p>No author info available</p>
+                        </>
+                }
+
+                <div className="list-group list-group-flush" style={{
+                    "maxHeight": "25vh",
+                    "maxWidth": "15vw",
+                    "overflowY": "scroll",
                 }}>
                     {(authorsList && topAuthors) ? topAuthors.map(
                         (authorScorePair) =>
                             <div className="list-group-item" key={authorScorePair["email"]}>
-                                <p className="small">{authorScorePair["email"]}: {formatSI(authorScorePair["contributionScore"])} ({formatPercentage(authorScorePair["relativeScore"])})</p>
+                                <p className="small text-break text-wrap">
+                                    {authorScorePair["email"]}
+                                </p>
+                                <h6 className="small">{formatSI(authorScorePair["contributionScore"])}</h6>
+                                <span className="small">({formatPercentage(authorScorePair["relativeScore"])})</span>
                             </div>
                     )
                         : <p className="small fw-bold">N/A</p>}
