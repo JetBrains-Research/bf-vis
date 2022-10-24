1247de12f827596dc655b47977f36edb0f7a1a37
Muhammad Umair Ahmed <muhdumairahmed@gmail.com>
2022-10-24 16:02:31 +0300

    Remove path modification that does not work

--- react-project/src/components/App.js
+++ react-project/src/components/App.js
@@ -1,19 +1,35 @@
+import { useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { selectCurrentStatsData, selectCurrentVisualizationData, selectCurrentVisualizationPath } from '../reducers/treemapSlice';
+import { scopeTreemapIn, selectCurrentStatsData, selectCurrentVisualizationData, selectCurrentVisualizationPath } from '../reducers/treemapSlice';
 import { selectExclusionFilters } from '../reducers/filterSlice';
 
 import LeftColumn from './Navigator';
 import TreeMap from './Treemap';
 import RightColumn from './RightColumn';
+import { payloadGenerator } from '../utils/reduxActionPayloadCreator';
+
 
 function App() {
     const dispatch = useDispatch();
-    
+
     const currentVisualizationData = useSelector(selectCurrentVisualizationData);
     const currentVisualizationPath = useSelector(selectCurrentVisualizationPath);
     const currentStatsData = useSelector(selectCurrentStatsData);
     const filters = useSelector(selectExclusionFilters);
+
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    // useEffect(() => {
+    //     const path = searchParams.get("path") || "";
+
+    //     if (path && path !== currentVisualizationPath) {
+    //         dispatch(scopeTreemapIn(payloadGenerator("path", path)));
+    //     }
+
+    // }, [searchParams, setSearchParams, currentVisualizationPath, dispatch])
+
 
     return (
         <div className="App container-fluid text-center">
--- react-project/src/components/Treemap.js
+++ react-project/src/components/Treemap.js
@@ -1,28 +1,17 @@
-import React, { useEffect, useReducer } from 'react';
+import React, { useEffect} from 'react';
 import { useSelector } from 'react-redux';
-import { useSearchParams } from 'react-router-dom';
 
-import { LAYOUT_CONSTANTS, treemapSvgId, treemapContainerId } from '../config';
+import { CONSTANTS, treemapSvgId, treemapContainerId } from '../config';
 
 import { selectAllFilters } from '../reducers/filterSlice';
 
 import { createSVGInContainer, clearCanvas } from '../d3/svgCanvas';
 import { generateTreemapLayoutFromData, drawTreemapFromGeneratedLayout } from '../d3/treemap';
-import { scopeTreemapIn } from '../reducers/treemapSlice';
-import { payloadGenerator } from '../utils/reduxActionPayloadCreator';
+
 
 function TreeMap(props) {
     const dispatch = props.dispatch;
     const filters = useSelector(selectAllFilters);
-    const [searchParams, setSearchParams] = useSearchParams();
-
-    useEffect(() => {
-        const path = searchParams.get("path") || "";
-
-        if (path && path !== props.data.path) {
-            dispatch(scopeTreemapIn(payloadGenerator("path", path)));
-        }
-    }, [searchParams, props.data.path])
 
     useEffect(() => {
 
@@ -33,22 +22,17 @@ function TreeMap(props) {
         clearCanvas(treemapSvgId);
 
         // Create SVG canvas
-        const svg = createSVGInContainer(`#${treemapContainerId}`, treemapSvgId, LAYOUT_CONSTANTS.treemap.height, LAYOUT_CONSTANTS.treemap.width);
+        const svg = createSVGInContainer(`#${treemapContainerId}`, treemapSvgId, CONSTANTS.treemap.layout.height, CONSTANTS.treemap.layout.width);
 
         // Loading and processing data
-        const rootNode = generateTreemapLayoutFromData(data, LAYOUT_CONSTANTS.treemap.height, LAYOUT_CONSTANTS.treemap.width, filters);
+        const rootNode = generateTreemapLayoutFromData(data, CONSTANTS.treemap.layout.height, CONSTANTS.treemap.layout.width, filters);
 
         // Drawing the treemap from the generated data
         drawTreemapFromGeneratedLayout(svg, rootNode, dispatch, filters);
 
-    }, [props.data, filters, dispatch, searchParams]
-    )
 
-    useEffect(() => {
-        setSearchParams({
-            "path": props.data.path,
-        })
-    }, [setSearchParams, props.data.path])
+    }, [props.data, filters, dispatch]
+    )
 
     return (
         <div id={treemapContainerId} className='container-fluid'>
