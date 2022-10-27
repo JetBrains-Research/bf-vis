4ad43dcdb103142929a0ad1644465222abf9cfeb
Muhammad Umair Ahmed <muhdumairahmed@gmail.com>
2022-10-27 10:23:46 +0300

    filter reducers in Redux

--- react-project/src/reducers/filterSlice.js
+++ react-project/src/reducers/filterSlice.js
@@ -42,9 +42,66 @@ const filterSlice = createSlice({
             const fileNamePrefixesToRemove = action.payload;
 
             if (Array.isArray(fileNamePrefixesToRemove) && fileNamePrefixesToRemove.length > 0) {
-                state.exclusion.fileNamePrefixes = state.exclusion.fileNamePrefixes.filter((element) => fileNamePrefixesToRemove.includes())
+                state.exclusion.fileNamePrefixes = state.exclusion.fileNamePrefixes.filter((element) => !fileNamePrefixesToRemove.includes(element))
             }
         },
+        addExclusionFilenameFilter: (state, action) => {
+            const fileNamesToAdd = action.payload;
+
+            if (Array.isArray(fileNamesToAdd) && fileNamesToAdd.length > 0) {
+                state.exclusion.fileNames = [...new Set(state.exclusion.fileNames.concat(fileNamesToAdd))]
+            }
+        },
+        removeExclusionFilenameFilter: (state, action) => {
+            const fileNamesToRemove = action.payload;
+
+            if (Array.isArray(fileNamesToRemove) && fileNamesToRemove.length > 0) {
+                state.exclusion.fileNames = state.exclusion.fileNames.filter((element) => !fileNamesToRemove.includes(element))
+            }
+        },
+        addInclusionExtensionsFilter: (state, action) => {
+            const newExtensions = action.payload;
+
+            if (Array.isArray(newExtensions) && newExtensions.length > 0) {
+                state.inclusion.extensions = [...new Set(state.inclusion.extensions.concat(newExtensions))]
+            }
+        },
+        removeInclusionExtensionsFilter: (state, action) => {
+            const extensionsToRemove = action.payload;
+
+            if (Array.isArray(extensionsToRemove) && extensionsToRemove.length > 0) {
+                state.inclusion.extensions = state.inclusion.extensions.filter((element) => !extensionsToRemove.includes(element))
+            }
+        },
+        addInclusionFilenamePrefixesFilter: (state, action) => {
+            const fileNamePrefixesToAdd = action.payload;
+
+            if (Array.isArray(fileNamePrefixesToAdd) && fileNamePrefixesToAdd.length > 0) {
+                state.inclusion.fileNamePrefixes = [...new Set(state.inclusion.fileNamePrefixes.concat(fileNamePrefixesToAdd))]
+            }
+        },
+        removeInclusionFilenamePrefixesFilter: (state, action) => {
+            const fileNamePrefixesToRemove = action.payload;
+
+            if (Array.isArray(fileNamePrefixesToRemove) && fileNamePrefixesToRemove.length > 0) {
+                state.inclusion.fileNamePrefixes = state.inclusion.fileNamePrefixes.filter((element) => !fileNamePrefixesToRemove.includes(element))
+            }
+        },
+        addInclusionFilenameFilter: (state, action) => {
+            const fileNamesToAdd = action.payload;
+
+            if (Array.isArray(fileNamesToAdd) && fileNamesToAdd.length > 0) {
+                state.inclusion.fileNames = [...new Set(state.inclusion.fileNames.concat(fileNamesToAdd))]
+            }
+        },
+        removeInclusionFilenameFilter: (state, action) => {
+            const fileNamesToRemove = action.payload;
+
+            if (Array.isArray(fileNamesToRemove) && fileNamesToRemove.length > 0) {
+                state.inclusion.fileNames = state.inclusion.fileNames.filter((element) => !fileNamesToRemove.includes(element))
+            }
+        },
+        
     }
 });
 
