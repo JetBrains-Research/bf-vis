10fb130cac26e97117ca2199886e18eff71d1e9a
Muhammad Umair Ahmed <muhdumairahmed@gmail.com>
2022-12-01 04:13:14 +0300

    add custom filter function

---
+++ react-project/src/utils/filter.js
@@ -0,0 +1,9 @@
+function filterElementFromArray (arr, itemToFilter) {
+    let result = []
+    for (var count = 0; count < result.length; count++) {
+        if (arr[count] !== itemToFilter) {
+            result.push(arr[count]);
+        }
+    }
+    return result
+}
